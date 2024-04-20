dda0b0c96b0beef96517e17e040355a61bf1d2c8
Egor Pilyugin <pilyugin06@bk.ru>
2024-04-20 22:11:41 +0300

    Почти пустой проект

---
+++ ConsoleAppForUnitTest/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForUnitTest {
+    internal class Calculator {
+
+
+        private int Sum(int firstSummand, int secondSummand) {
+            return firstSummand + secondSummand;
+        }
+    }
+}
