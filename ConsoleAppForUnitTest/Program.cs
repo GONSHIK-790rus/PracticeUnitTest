20d62f5c81e3b1183f88efa00c0a2d96b8090efb
Egor Pilyugin <pilyugin06@bk.ru>
2024-04-20 23:20:14 +0300

    Доделал код (вроде) :D

--- ConsoleAppForUnitTest/Program.cs
+++ ConsoleAppForUnitTest/Program.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Библиотеки
+using System;
+#endregion
 
 namespace ConsoleAppForUnitTest {
     internal class Program {
         static void Main() {
+            #region Поля
             Calculator calculator = new Calculator();
             int firstNumber;
             int secondNumber;
             int buf;
-
-            Console.WriteLine("Добро пожаловать в калькулятор.");
+            #endregion
 
             EnterNumsAnchor:
+            Console.WriteLine("Добро пожаловать в калькулятор.");
+
             do {
                 Console.Write("Введите первое число: ");
             }  while (!int.TryParse(Console.ReadLine(), out firstNumber));
@@ -24,6 +24,7 @@ namespace ConsoleAppForUnitTest {
             } while (!int.TryParse(Console.ReadLine(), out secondNumber));
 
             ChooseActionAnchor:
+            #region "Выберете действие"
             // Вырвиглазно но пардон))
             Console.WriteLine("Выберете действие:");
             Console.WriteLine($"1 - Сложить ({firstNumber} + {secondNumber})");
@@ -33,35 +34,52 @@ namespace ConsoleAppForUnitTest {
             Console.WriteLine($"5 - Найти остаток от деления ({firstNumber} / {secondNumber})");
             Console.WriteLine($"6 - Поменять числа местами ({secondNumber} и {firstNumber})");
             Console.WriteLine($"7 - Ввести новые числа");
-            switch (Console.ReadLine()) {
-                case "1":
+            #endregion
+            switch (Console.ReadKey().Key) {
+                case ConsoleKey.D1:
+                    Console.WriteLine();
+                    Console.WriteLine($"Результат вычислений = {calculator.Sum(firstNumber, secondNumber)}");
                     break;
 
-                case "2":
+                case ConsoleKey.D2:
+                    Console.WriteLine();
+                    Console.WriteLine($"Результат вычислений = {calculator.Subtract(firstNumber, secondNumber)}");
                     break;
 
-                case "3":
+                case ConsoleKey.D3:
+                    Console.WriteLine();
+                    Console.WriteLine($"Результат вычислений = {calculator.Multiply(firstNumber, secondNumber)}");
                     break;
 
-                case "4":
+                case ConsoleKey.D4:
+                    Console.WriteLine();
+                    Console.WriteLine($"Результат вычислений = {calculator.Divide(firstNumber, secondNumber)}");
                     break;
 
-                case "5":
+                case ConsoleKey.D5:
+                    Console.WriteLine();
                     Console.WriteLine($"Результат вычислений = {calculator.Remainder(firstNumber, secondNumber)}");
                     break;
 
-                case "6":
+                case ConsoleKey.D6:
+                    // Будем откровенны: я вообще не заморачивался :D
                     buf = firstNumber;
                     firstNumber = secondNumber;
                     secondNumber = buf;
+                    Console.Clear();
+                    Console.WriteLine("Добро пожаловать в калькулятор.");
                     goto ChooseActionAnchor;
 
-                case "7":
+                case ConsoleKey.D7:
+                    Console.Clear();
                     goto EnterNumsAnchor;
 
                 default:
                     goto ChooseActionAnchor;
             }
+
+            Console.WriteLine("Нажмите клавишу \"Enter\", чтобы выйти");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
 }
